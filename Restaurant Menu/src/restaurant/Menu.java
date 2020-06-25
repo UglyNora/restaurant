f3bb5caf9f1d3b50453a277e583a7dc2e9e64579
uglynora <mangos8@yahoo.com>
2020-06-25 19:05:31 -0400

    Modified Menu.java and MenuItem.java

--- Restaurant Menu/src/restaurant/Menu.java
+++ Restaurant Menu/src/restaurant/Menu.java
@@ -1,4 +1,13 @@
 package restaurant;
+//import java.util.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class Menu {
+    public static void main(String args[]){
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+    LocalDateTime now = LocalDateTime.now();
+    System.out.println("Welcome to It Ain't No Good- Food Truck! " +
+            " Menu for : "+ dtf.format(now));
+    }
 }
--- Restaurant Menu/src/restaurant/MenuItem.java
+++ Restaurant Menu/src/restaurant/MenuItem.java
@@ -1,4 +1,51 @@
 package restaurant;
 
+
 public class MenuItem {
+
+    private String category;
+    private String description;
+    private double price;
+    private boolean isNew;
+
+    public MenuItem(String category, String description, double price, boolean isNew) {
+       this.category = category;
+       this.description = description;
+       this.price = price;
+       this.isNew = false;
+
+
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getcategory() {
+        return category;
+    }
+
+    public void setcategory(String category) {
+        category = category;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
 }
